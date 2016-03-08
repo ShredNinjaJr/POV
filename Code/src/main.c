9b2bf1b54202e015a847d261a9e49e063567c4e9
unknown <vemulpt2@ECEB-2076-09.ad.uillinois.edu>
2016-03-08 01:29:28 -0600

    Added Pin labels in main.c

--- Code/src/main.c
+++ Code/src/main.c
@@ -2,15 +2,49 @@
 #include "LPC11xx.h"
 #include "GPIO.h"
 
+#define PIN_1 PIO2_6
+#define PIN_2 PIO2_0
+#define PIN_4 PIO0_1
+#define PIN_9 PIO1_8
+#define PIN_10 PIO0_2
+#define PIN_11 PIO2_7
+#define PIN_12 PIO2_8
+#define PIN_13 PIO2_1
+#define PIN_14 PIO0_3
+#define PIN_15 PIO0_4
+#define PIN_16 PIO0_5
+#define PIN_17 PIO1_9
+#define PIN_18 PIO3_4
+#define PIN_19 PIO2_4
+#define PIN_20 PIO2_5
+#define PIN_21 PIO3_5
+#define PIN_22 PIO0_6
+#define PIN_23 PIO0_7
+#define PIN_24 PIO2_9
+#define PIN_25 PIO2_10
+#define PIN_26 PIO2_2
+#define PIN_27 PIO0_8
+#define PIN_28 PIO0_9
+#define PIN_30 PIO1_10
+#define PIN_31 PIO2_11
+#define PIN_36 PIO3_0
+#define PIN_37 PIO3_1
+#define PIN_38 PIO2_3
+#define PIN_40 PIO1_4
+#define PIN_42 PIO1_11
+#define PIN_43 PIO3_2
+#define PIN_45 PIO1_5
+#define PIN_48 PIO3_3
+
 
 int main()
 {
-	const GPIO_pin ledPin = PIO2_1;
+	const GPIO_pin ledPin = PIN_23;
 	const GPIO_pin buttonPin = PIO1_9;
 	configureGPIO(ledPin, OUTPUT);
 	configureGPIO(buttonPin, INPUT);
 	while (1)
 	{
-		GPIO_Write(ledPin,GPIO_Read(buttonPin));
+		GPIO_Write(ledPin, 1);//GPIO_Read(buttonPin));
 	}
 }
