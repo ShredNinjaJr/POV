a4c9a331fecb0671aa97d11ab85eb874f528fbbd
unknown <vemulpt2@ECEB-2076-09.ad.uillinois.edu>
2016-03-15 16:34:40 -0500

    Added font_table

--- Code/src/main.c
+++ Code/src/main.c
@@ -1,50 +1,39 @@
 #include <stdio.h>
 #include "LPC11xx.h"
 #include "GPIO.h"
+#include "font_table.h"
 
-#define PIN_1 PIO2_6
-#define PIN_2 PIO2_0
-#define PIN_4 PIO0_1
-#define PIN_9 PIO1_8
-#define PIN_10 PIO0_2
-#define PIN_11 PIO2_7
-#define PIN_12 PIO2_8
-#define PIN_13 PIO2_1
-#define PIN_14 PIO0_3
-#define PIN_15 PIO0_4
-#define PIN_16 PIO0_5
-#define PIN_17 PIO1_9
-#define PIN_18 PIO3_4
-#define PIN_19 PIO2_4
-#define PIN_20 PIO2_5
-#define PIN_21 PIO3_5
-#define PIN_22 PIO0_6
-#define PIN_23 PIO0_7
-#define PIN_24 PIO2_9
-#define PIN_25 PIO2_10
-#define PIN_26 PIO2_2
-#define PIN_27 PIO0_8
-#define PIN_28 PIO0_9
-#define PIN_30 PIO1_10
-#define PIN_31 PIO2_11
-#define PIN_36 PIO3_0
-#define PIN_37 PIO3_1
-#define PIN_38 PIO2_3
-#define PIN_40 PIO1_4
-#define PIN_42 PIO1_11
-#define PIN_43 PIO3_2
-#define PIN_45 PIO1_5
-#define PIN_48 PIO3_3
+/* Header for pin numbers for LPC1114FBD48/302 */
+#include "LPC1114FBD48_302_label.h"
 
 
 int main()
 {
 	const GPIO_pin ledPin = PIN_23;
 	const GPIO_pin buttonPin = PIO1_9;
+	const GPIO_pin hall_effect_pin = PIN_13;
 	configureGPIO(ledPin, OUTPUT);
 	configureGPIO(buttonPin, INPUT);
+	configureGPIO(hall_effect_pin, INPUT);
 	while (1)
 	{
-		GPIO_Write(ledPin, 1);//GPIO_Read(buttonPin));
+		GPIO_Write(ledPin, GPIO_Read(hall_effect_pin)^ GPIO_Read(buttonPin));
 	}
 }
+
+
+/* 
+
+font_table[char][height][width]
+for each char in the string: 
+	for(i = 0; i < width; i++)
+	{
+		for(j = 0; j < height; j++)
+		{
+				led[j] = char[j][i];
+		}
+		delay(x);
+	}
+
+
+*/
