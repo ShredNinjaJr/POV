bd38c25700447e74ef1d18621b03c76d4218472b
unknown <vemulpt2@ECEB-2076-09.ad.uillinois.edu>
2016-04-04 13:32:56 -0500

    Got font table printing in serial

--- Code/src/main.c
+++ Code/src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "LPC11xx.h"
 #include "GPIO.h"
 #include "font_table.h"
@@ -6,18 +7,42 @@
 /* Header for pin numbers for LPC1114FBD48/302 */
 #include "LPC1114FBD48_302_label.h"
 
+/* Import external functions from Serial.c file                               */
+extern void SER_init (void);
 
 int main()
 {
 	const GPIO_pin ledPin = PIN_23;
 	const GPIO_pin buttonPin = PIO1_9;
 	const GPIO_pin hall_effect_pin = PIN_13;
+	int i, j, k;
+	int height = 8, width = 8;
+	char input[] = "Sahil, I am your father!";
+	SER_init();
+	
 	configureGPIO(ledPin, OUTPUT);
 	configureGPIO(buttonPin, INPUT);
 	configureGPIO(hall_effect_pin, INPUT);
+	
+	
+	
 	while (1)
 	{
-		GPIO_Write(ledPin, GPIO_Read(hall_effect_pin)^ GPIO_Read(buttonPin));
+		for(i =0; i < strlen(input); i++)
+		{
+			for(j = 0; j < height; j++)
+			{
+					for( k = 0; k < width; k++)
+					{
+							char temp = font_table[input[i] - ' '][j];
+							if(temp & (0x80 >> k))
+								printf("1");
+							else
+								printf(" ");
+					}
+					printf("\r\n");
+			}
+		}
 	}
 }
 
@@ -30,9 +55,9 @@ for each char in the string:
 	{
 		for(j = 0; j < height; j++)
 		{
-				led[j] = char[j][i];
+				led[j] = font_table[char - ' '][j][i];
 		}
-		delay(x);
+		delay(x);		
 	}
 
 
