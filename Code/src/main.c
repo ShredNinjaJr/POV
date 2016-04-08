2187dd34a3c59bcdbd721dd4b342d21ec174c04c
unknown <vemulpt2@ECEB-2076-09.ad.uillinois.edu>
2016-04-08 18:40:36 -0500

    LED pattern works, need to write timing

--- Code/src/main.c
+++ Code/src/main.c
@@ -3,6 +3,7 @@
 #include "LPC11xx.h"
 #include "GPIO.h"
 #include "font_table.h"
+#include "time.h"
 
 /* Header for pin numbers for LPC1114FBD48/302 */
 #include "LPC1114FBD48_302_label.h"
@@ -10,19 +11,32 @@
 /* Import external functions from Serial.c file                               */
 extern void SER_init (void);
 
+const GPIO_pin ledPins[] = {PIN_13, PIN_14, PIN_15, PIN_16, 
+														PIN_17, PIN_18, PIN_19, PIN_20,
+											      PIN_21, PIN_22, PIN_23, PIN_24
+													 };
+
 int main()
 {
-	const GPIO_pin ledPin = PIN_23;
-	const GPIO_pin buttonPin = PIO1_9;
-	const GPIO_pin hall_effect_pin = PIN_13;
-	int i, j, k;
-	int height = 8, width = 8;
-	char input[] = "Sahil, I am your father!";
-	SER_init();
+	const GPIO_pin ledPin = PIN_13;
 	
-	configureGPIO(ledPin, OUTPUT);
+	const GPIO_pin buttonPin = PIN_25;
+	const GPIO_pin hall_effect_pin = PIN_13;
+	int i, j, k, l;
+	int height = 8, width = 8;
+	char input[] = "Hello World!";
+	SER_init();
+	for( i = 0; i < 12; i++)
+		configureGPIO(ledPins[i], OUTPUT);
+	
+	for( i = 0; i < 12; i++)
+		GPIO_Write(ledPins[i], 0);
+	
 	configureGPIO(buttonPin, INPUT);
 	configureGPIO(hall_effect_pin, INPUT);
+	#define DELAY 30000
+
+	
 	
 	
 	
@@ -30,20 +44,21 @@ int main()
 	{
 		for(i =0; i < strlen(input); i++)
 		{
-			for(j = 0; j < height; j++)
+			for(j = 0; j < width; j++)
 			{
-					for( k = 0; k < width; k++)
+					for( k = 0; k < height; k++)
 					{
-							char temp = font_table[input[i] - ' '][j];
-							if(temp & (0x80 >> k))
-								printf("1");
-							else
-								printf(" ");
+							char temp = font_table[input[i] - ' '][k];
+							GPIO_Write(ledPins[k],(temp & (0x80 >> j)));
+						//printf("%d\r\n", (temp & (0x80 >> j))? 1 : 0);
 					}
-					printf("\r\n");
+					
+					for( l = 0; l < DELAY; l++);
 			}
+			
 		}
 	}
+	
 }
 
 
