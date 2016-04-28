dec69b34c00f5751bfe519fcf5bbda8574884648
unknown <vemulpt2@ECEB-2076-09.ad.uillinois.edu>
2016-04-28 15:31:14 -0500

    Implemented the timer

--- Code/src/main.c
+++ Code/src/main.c
@@ -8,6 +8,7 @@
 /* Header for pin numbers for LPC1114FBD48/302 */
 #include "LPC1114FBD48_302_label.h"
 
+#define PROCESSOR_FREQ   50000000
 /* Import external functions from Serial.c file                               */
 extern void SER_init (void);
 
@@ -19,32 +20,58 @@ const GPIO_pin ledPins[] = {PIN_13, PIN_14, PIN_15, PIN_16,
 void PIOINT2_IRQHandler(void);
 volatile int intr = 0;
 
-int i, j, k, l;
-	int height = 8, width = 8;
-	char input[] = "H";												
-	#define DELAY 3000
+volatile int counter = 0;
+volatile int delay_counter = 0;
 													 
+int height = 8, width = 8;
+char input[] = "Hello!";												
+int DELAY = 3000;
+													 
+#define QUEUE_SIZE 10
+struct queue
+{
+		int data[QUEUE_SIZE];
+		int write_idx;
+} timer_queue;
+						
+
 void PIOINT2_IRQHandler(void)
 {
-		LPC_GPIO2->IE &= ~(1 << 8);
-		for(i =0; i < strlen(input); i++)
-		{
-			for(j = 0; j < width; j++)
-			{
-					for( k = 0; k < height; k++)
-					{
-							char temp = font_table[input[i] - ' '][k];
-							GPIO_Write(ledPins[k],(temp & (0x80 >> j)));
-						//printf("%d\r\n", (temp & (0x80 >> j))? 1 : 0);
-					}
-					
-					for( l = 0; l < DELAY; l++);
-			}
-			
-		}
-		for(k = 0; k < height; k++)
-				GPIO_Write(ledPins[k],0);
-		LPC_GPIO2->IE |= (1 << 8);
+		LPC_GPIO2->IC |= (1 << 8);
+		
+		timer_queue.data[timer_queue.write_idx] = counter;
+		timer_queue.write_idx = (timer_queue.write_idx + 1) % QUEUE_SIZE;
+		counter = 0;
+}
+
+
+void SysTick_Handler (void)
+{
+	counter++;
+	delay_counter++;
+}
+
+void delay(int time)
+{
+		delay_counter = 0;
+		
+		while(delay_counter < time);
+	
+		return;
+}
+
+void InitializeSysTickTimer(int frequency)
+{
+	/* Program the SYST_RVR register with the reload value RELOAD to obtain the desired
+time interval.*/
+	SysTick->LOAD = (PROCESSOR_FREQ / frequency) - 1;	/* INITIALIZED TO 1 millisecond*/
+	printf("SYSTick counter = %d\r\n",(PROCESSOR_FREQ / frequency) - 1);
+	/*Clear the SYST_CVR register*/
+	SysTick->VAL = 0;
+	/* Program the SYST_SCR register with the value 0x7 which enables the SysTick timer
+and the SysTick timer interrupt.*/
+	SysTick->CTRL |= 0x7;
+	
 }
 													 
 int main()
@@ -53,7 +80,8 @@ int main()
 	
 	const GPIO_pin buttonPin = PIN_25;
 	const GPIO_pin hall_effect_pin = PIN_12;
-	
+	int i, j, k, l;
+	int MSPR = 0;
 	SER_init();
 	for( i = 0; i < 12; i++)
 		configureGPIO(ledPins[i], OUTPUT);
@@ -63,56 +91,59 @@ int main()
 	
 	configureGPIO(buttonPin, INPUT);
 	configureGPIO(hall_effect_pin, INPUT);
-
-		/* Unmask the interrupt */
-	LPC_GPIO2->IE |= (1 << 8);
-	LPC_GPIO2->IS |= (1 << 8);
-	NVIC_EnableIRQ(EINT2_IRQn);
+	InitializeSysTickTimer(1000);
+	/* Initialize timer_queue */
+	for(i = 0; i < QUEUE_SIZE; i++)
+	{
+			timer_queue.data[i] = 0;
+	}
+	timer_queue.write_idx = 0;
 	
-	#ifdef as
+	
+	#ifndef INTERRUPT
+	/* Enable interrupts on PIO2_1 */
+	
+	/* Unmask the interrupt */
+	LPC_GPIO2->IE |= (1 << 8);
+	LPC_GPIO2->IS &= ~(1 << 8);
+	NVIC_EnableIRQ(EINT2_IRQn);
+		
+	#endif
+	
+	
+	#ifndef INTERRUPT
 	while (1)
 	{
-		/*
+		
 		for(k = 0; k < height; k++)
 				GPIO_Write(ledPins[k],0);
-		while(LPC_GPIO2->DATA & (1 << 8));
-		for(k = 0; k < height; k++)
-				GPIO_Write(ledPins[k],1);
 		
-		for( l = 0; l < DELAY; l++);
-		*/
-		while(!intr);
-		intr = 0;
+		while(LPC_GPIO2->DATA & (1 << 8));
 		for(i =0; i < strlen(input); i++)
 		{
 			for(j = 0; j < width; j++)
 			{
-					fors( k = 0; k < height; k++)
+					for( k = 0; k < height; k++)
 					{
 							char temp = font_table[input[i] - ' '][k];
-							GPIO_Write(ledPins[k],(temp & (0x80 >> j)));
+							GPIO_Write(ledPins[k],1);//(temp & (0x80 >> j)));
 						//printf("%d\r\n", (temp & (0x80 >> j))? 1 : 0);
 					}
-					
-					for( l = 0; l < DELAY; l++);
+					/* Calculate the Rotation time */
+					MSPR = 0;
+					for(l = 0; l < QUEUE_SIZE; l++)
+					{
+							MSPR +=	timer_queue.data[i];
+					}
+					MSPR /= QUEUE_SIZE;
+					delay(MSPR/2);
 			}
 			
 		}
 	}
 	#endif
 	
-	#ifdef INTERRUPT
-	/* Enable interrupts on PIO2_1 */
-	
-	/* Unmask the interrupt */
-	LPC_GPIO2->IE |= (1 << 8);
-	LPC_GPIO2->IS |= (1 << 8);
-	NVIC_EnableIRQ(EINT2_IRQn);
-	
-	
-	
-		
-	#endif
+
 	
 }
 
